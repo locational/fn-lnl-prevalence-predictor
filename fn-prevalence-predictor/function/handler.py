5a7f0dd8339a470cb39e13599a419e354d42e39e
Jonathan Smith <jonathan@peoplesized.com>
2019-03-11 12:14:53 +0200

    remove debug lines

--- fn-prevalence-predictor/function/handler.py
+++ fn-prevalence-predictor/function/handler.py
@@ -1,31 +1,31 @@
-import json
 import sys
 import pandas as pd
 import numpy as np
 from disarm_gears.chain_drives.prototypes import adaptive_prototype_0
 
+
 def run_function(params):
-    """handle a request to the function
-    Args:
-        req (str): request body
-    """
-    region_data = pd.DataFrame(params['region_definition'])
-    train_data = pd.DataFrame(params['train_data'])
-    sys.stderr.write("DEBUGGING region_data: " + str(region_data))                                    
-    sys.stderr.write("DEBUGGING train_data: " + str(train_data))                                    
+    # Extract required params
+    threshold = params['request_parameters']['threshold']
+    region_definition = params['region_definition']
+    train_data = params['train_data']
 
+    # Create initial dataframes
+    region_data = pd.DataFrame(region_definition)
+    train_data = pd.DataFrame(train_data)
 
+    #
     x_frame = np.array(region_data[['lng', 'lat']])
     x_id = np.array(region_data['id'])
     x_coords = np.array(train_data[['lng', 'lat']])
     n_trials = np.array(train_data['n_trials'])
     n_positive = np.array(train_data['n_positive'])
-    threshold = params['request_parameters']['threshold']
 
+    # Start the hard bit
+    sys.stderr.write("Starting heavy bit...")
     response = adaptive_prototype_0(x_frame=x_frame, x_id=x_id,
                                     x_coords=x_coords,
                                     n_positive=n_positive,
                                     n_trials=n_trials,
-                                    threshold=threshold)#, covariate_layers=None)
-    sys.stderr.write("DEBUGGING response: " + str(response))                                    
+                                    threshold=threshold)  # , covariate_layers=None)
     return response
