d3a1e84319d01ab9d8180639e07e18ef69a76674
Jonathan Smith <jonathan@peoplesized.com>
2019-03-11 15:38:33 +0200

    rename original, replace with new from template

---
+++ fn-prevalence-predictor/preprocess_params.py
@@ -0,0 +1,7 @@
+from preprocess_helpers import write_temp_from_url_or_base64, required_exists
+
+# Receives the params dict
+# Throw an error any time a test for a param fails.
+# Otherwise, return nothing
+def preprocess(params: dict):
+    raise ValueError('Need to implement `preprocess` steps in function/preprocess_params.py')
