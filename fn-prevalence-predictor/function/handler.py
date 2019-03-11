2f9307d5d35d9fe5c99ef476f9fbefa58bc66f0f
Jonathan Smith <jonathan@peoplesized.com>
2019-03-11 11:03:20 +0200

    handler already gets a dict

--- fn-prevalence-predictor/function/handler.py
+++ fn-prevalence-predictor/function/handler.py
@@ -4,21 +4,20 @@ import pandas as pd
 import numpy as np
 from disarm_gears.chain_drives.prototypes import adaptive_prototype_0
 
-def run_function(req):
+def run_function(params):
     """handle a request to the function
     Args:
         req (str): request body
     """
-    json_data = json.loads(req)
-    region_data = pd.DataFrame(json_data['region_definition'])
-    train_data = pd.DataFrame(json_data['train_data'])
+    region_data = pd.DataFrame(params['region_definition'])
+    train_data = pd.DataFrame(params['train_data'])
 
     x_frame = np.array(region_data[['lng', 'lat']])
     x_id = np.array(region_data['id'])
     x_coords = np.array(train_data[['lng', 'lat']])
     n_trials = np.array(train_data['n_trials'])
     n_positive = np.array(train_data['n_positive'])
-    threshold = json_data['request_parameters']['threshold']
+    threshold = params['request_parameters']['threshold']
 
     response = adaptive_prototype_0(x_frame=x_frame, x_id=x_id,
                                     x_coords=x_coords,
