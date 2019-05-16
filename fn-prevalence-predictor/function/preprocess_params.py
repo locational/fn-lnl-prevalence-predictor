c172039a71f0d6ab84d1298d67292ae2f2589146
Jonathan Smith <jonathan@peoplesized.com>
2019-05-16 11:36:47 +0200

    example of preprocess-params that we’ll need (once specs are confirmed)

--- fn-prevalence-predictor/function/preprocess_params.py
+++ fn-prevalence-predictor/function/preprocess_params.py
@@ -3,3 +3,5 @@ from preprocess_helpers import write_temp_from_url_or_base64, required_exists
 
 def preprocess(params: dict):
     type(params['request_parameters']['threshold']) in [float, int]
+
+    required_exists('train_data', params)
