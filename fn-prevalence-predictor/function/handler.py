e405694459a3517a2e4d25f9918f3fde2173d2a6
ric70x7 <ric70x7@gmail.com>
2019-05-17 12:54:12 -0700

    merge bug fixed

--- fn-prevalence-predictor/function/handler.py
+++ fn-prevalence-predictor/function/handler.py
@@ -5,30 +5,6 @@ import pandas as pd
 import requests
 import disarm_gears
 
-def geojson_encoder_tmp(dataframe, fields=[], layer_names=[], lng='lng', lat='lat'):
-    '''
-    Put dataframe into geojson format
-
-    :param dataframe: Data to put into geojson format.
-                      pandas DataFrame.
-    :param layer_names:
-                        List of strings.
-    :param lng: Name of longitude variable within dataframe.
-                String, defaults to 'lng'.
-    :param lat: Name of latitude variable within dataframe.
-                String, defaults to 'lat'.
-    '''
-    #TODO add tests
-    geojson_dict = {'type': 'FeatureCollection', 'features': []}
-    for _, row in dataframe.iterrows():
-        _feature = {'type': 'Feature', 'properties': row[fields].to_dict(), 'geometry': {'type': 'Point', 'coordinates': []}}
-        _feature['geometry']['coordinates'] = [row[lng], row[lat]]
-        geojson_dict['features'].append(_feature)
-
-    new_json = {'layer_names': layer_names, 'points': geojson_dict}
-
-    return json.dumps(new_json)
-
 
 def run_function(params: dict):
     #
@@ -58,14 +34,14 @@ def run_function(params: dict):
     if layer_names is not None:
         # Call fn-covariate-extractor
         open_faas_link = 'http://faas.srv.disarm.io/function/fn-covariate-extractor'
-        covs_request = geojson_encoder_tmp(input_data, fields=['id'], layer_names=layer_names)
+        covs_request = disarm_gears.util.geojson_encoder_3(input_data, fields=['id'], layer_names=layer_names, dumps=True)
         covs_response = requests.post(open_faas_link, data=covs_request)
         # TODO? assert covs_response.json()['type'] == 'success'
         # TODO define how to handle NA entries in the covariates
 
         # Merge output into input_data
         covs_data = disarm_gears.util.geojson_decoder_1(covs_response.json()['result'])
-        input_data = pd.merge(input_data, covs_data, how='left', left_on=['id'], right_on=['id'])
+        input_data = pd.merge(input_data, covs_data[['id'] + layer_names], how='left', left_on=['id'], right_on=['id'])
         #for li in layer_names:
         #    input_data[li] = covs_data[li]
 
