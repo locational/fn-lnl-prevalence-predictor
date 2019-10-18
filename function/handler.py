3a0857909419b43c433fbcc14d8df31957e1358d
Jonathan Smith <jonathan@peoplesized.com>
2019-10-18 11:47:48 +0200

    remove prevalence from return and SPECS

--- function/handler.py
+++ function/handler.py
@@ -101,8 +101,8 @@ def run_function(params: dict):
     # 3. Package output
     #
 
-    input_data['prevalence_prediction'] = gam_pred
-    input_data['prevalence_bci_width'] = bci[1] - bci[0]
+    input_data['prediction'] = gam_pred
+    input_data['bci_width'] = bci[1] - bci[0]
     input_data['exceedance_probability'] = ex_prob
     input_data['exceedance_uncertainty'] = ex_uncert
 
